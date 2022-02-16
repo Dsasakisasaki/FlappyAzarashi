95c1da5a2933d426043f262b0b9792807fad4f5a
sasaki <kunnrenn0909@gmail.com>
2022-02-16 13:48:07 +0900

    完成

---
+++ Assets/Script/ClearTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearTrigger : MonoBehaviour
+{
+    GameObject gameController;
+
+    private void Start()
+    {
+        //
+        gameController = GameObject.FindWithTag("GameController");
+    }
+
+    //
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        gameController.SendMessage("IncreaseScore");
+    }
+}
